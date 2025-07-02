654a6296c0a058c7f648d12913192ce3beda6f65
hehe <irinapoltorakova404@gmail.com>
2025-07-03 02:09:14 +0300

    add project structure

---
+++ backend/user-service/internal/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import "time"
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8081"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
---
+++ backend/user-service/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"./internal/config"
+)
+
+func main() {
+	// Загружаем конфиг
+	cfg := config.Config{
+		HTTPServer: config.HTTPServer{
+			Address:     "0.0.0.0:8081", // порт сервиса
+			Timeout:     5 * time.Second,
+			IdleTimeout: 60 * time.Second,
+		},
+	}
+
+	// Роутер
+	mux := http.NewServeMux()
+
+	// Простейший эндпоинт
+	mux.HandleFunc("/api/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "pong from activity-service")
+	})
+
+	// Создаем сервер
+	srv := &http.Server{
+		Addr:         cfg.HTTPServer.Address,
+		Handler:      mux,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	log.Printf("Starting server on %s", cfg.HTTPServer.Address)
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %s", err)
+	}
+}
